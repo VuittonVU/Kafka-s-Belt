ba69c0b289b7371cc8fe8fd9e39d29abc361e9e1
JasonSanjaya <145695216+JasonSanjaya@users.noreply.github.com>
2025-12-08 22:11:58 +0700

    comment in playercontroller.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        cam = Camera.main;
+        cam = Camera.main;//biar tau kamera mana yang digunakan raycast mouse
     }
 
     void Update()
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");   // A / D
         float v = Input.GetAxisRaw("Vertical");     // W / S
 
-        Vector3 moveDir = new Vector3(h, 0f, v).normalized;
+        Vector3 moveDir = new Vector3(h, 0f, v).normalized;//normalized biar kecepatan tetap sama walaupun gerak diagonal 
 
         // Manual transform movement
         transform.position += moveDir * moveSpeed * Time.deltaTime;
@@ -31,17 +31,17 @@ public class PlayerController : MonoBehaviour
 
     void RotateToMouse()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);//dimulai dari kamera, menuju posisi mouse di layar.
 
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             Vector3 lookDir = hit.point - transform.position;
             lookDir.y = 0f; // biar ga miring ke atas/bawah
 
-            if (lookDir != Vector3.zero)
+            if (lookDir != Vector3.zero)//membuat player hadap ke mana
             {
                 Quaternion targetRot = Quaternion.LookRotation(lookDir);
-                transform.rotation = Quaternion.Slerp(
+                transform.rotation = Quaternion.Slerp(//slerp biar smooth
                     transform.rotation,
                     targetRot,
                     rotationSpeed * Time.deltaTime
@@ -50,3 +50,4 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
+
